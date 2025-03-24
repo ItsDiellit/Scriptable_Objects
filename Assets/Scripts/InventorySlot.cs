b0eea426d4c1a1213024acf0e991999941877f25
ItsDiellit <albadsdg@gmail.com>
2025-03-24 12:55:04 +0100

    inventario

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -26,8 +26,13 @@ public class InventorySlot : MonoBehaviour
 
     void InspectItem()
     {
-        if(slotItem != null && inspectionWindow.activeInHierarchy)
+        if(slotItem != null && !inspectionWindow.activeInHierarchy)
         {
+
+            deleteButton.onClick.AddListener(RemoveItem);
+            closeButton.onClick.AddListener(CloseWindow);
+
+            
             inspectionImage.sprite = slotItem.itemSprite;
             inspectionName.text = slotItem.itemName;
             inspectionDescription.text = slotItem.itemDescription;
@@ -35,5 +40,23 @@ public class InventorySlot : MonoBehaviour
             inspectionWindow.SetActive(true);
         }
     }
+
+    void RemoveItem()
+   {
+        if(InventoryManager.instance.weapons[slotNumber] != null)
+        {
+            InventoryManager.instance.weapons[slotNumber] = null;
+            InventoryManager.instance.weaponsNames[slotNumber].text = "Empty";
+            InventoryManager.instance.weaponsSprites[slotNumber].sprite = null;
+        }
+
+        CloseWindow();
+   }
+
+   void CloseWindow()
+   {
+    deleteButton.onClick.RemoveListener(RemoveItem);
+        inspectionWindow.SetActive(false);
+   }
 
 }
